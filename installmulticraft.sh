4d91d3aa694cc2aaa53e3909564bd5ea0b5a7173
mcmyhost <info@mcmyhost.net>
2013-12-13 17:51:50 -0500

    Create installmulticraft.sh

---
+++ installmulticraft.sh
@@ -0,0 +1,18 @@
+apt-get update
+ 
+apt-get install default-jdk php5 php5-mysql -y
+ 
+wget http://www.multicraft.org/download?arch=linux64 -O multicraft.tar.gz
+ 
+tar xvzf multicraft.tar.gz
+ 
+cd multicraft
+ 
+echo y
+y
+y
+no
+1
+ 
+ 
+ | ./setup.sh
